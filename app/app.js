eb22d34a4c79a8de46302a813c76b97fddf1af10
Greyson Hill <gthill7@gmail.com>
2017-01-15 23:24:54 -0500

    Building structure

---
+++ app/app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var reload = require('reload');
+var app = express();
+
+app.set('port', process.env.PORT || 3000 );
+app.set('view engine', 'ejs');
+
+app.use(express.static('app/public'));
+app.use(require('./routes/index'));
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Listening on port ' + app.get('port'));
+});
+
+reload(server, app);
