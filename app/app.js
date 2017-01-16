15ba4938062300f444ca4c6e22282f0b904c932f
Greyson Hill <gthill7@gmail.com>
2017-01-15 23:40:28 -0500

    Added charger image, created route for home page

--- app/app.js
+++ app/app.js
@@ -4,6 +4,7 @@ var app = express();
 
 app.set('port', process.env.PORT || 3000 );
 app.set('view engine', 'ejs');
+app.set('views', 'app/views');
 
 app.use(express.static('app/public'));
 app.use(require('./routes/index'));
